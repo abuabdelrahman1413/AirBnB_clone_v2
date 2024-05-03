4ff5b50bca1d8b6ff84e1ba460e45f2870637cee
AbduHany <abdu.hany@gmail.com>
2024-05-03 16:38:29 -0700

    Add 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,3 +26,38 @@ def do_pack():
     if (exists(path)):
         return path
     return None
+
+
+def do_deploy(archive_path):
+    """This function distributes an archive to your web servers.
+    """
+    if (isfile(archive_path) is False):
+        return False
+    name_tgz = archive_path.split('/')[-1]
+    name_notgz = name_tgz.split('.')[0]
+    try:
+        put(archive_path, "/tmp/")
+        sudo("rm -rf /data/web_static/releases/{}/".format(name_notgz))
+        sudo("mkdir -p /data/web_static/releases/{}/".format(name_notgz))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            name_tgz, name_notgz))
+        sudo("rm /tmp/{}".format(name_tgz))
+        sudo(("mv /data/web_static/releases/{}/web_static/* " +
+              "/data/web_static/releases/{}/").format(
+                  name_notgz, name_notgz))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(
+             name_notgz))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+             .format(name_notgz))
+        return True
+    except Exception:
+        return False
+
+def deploy():
+    """This function created and distributes an architec to my web servers.
+    """
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(architve_path=path)
