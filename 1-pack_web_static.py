32a09685b695697b2856f947365f70baa9578f9b
AbduHany <abdu.hany@gmail.com>
2024-05-03 13:23:51 -0700

    Modify 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This script generates a .tgz archive from the contents of the web_static folder of
-your AirBnB Clone repo, using the function do_pack
+"""This script generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the
+function do_pack
 """
 from fabric.api import *
 from datetime import datetime
@@ -8,11 +9,12 @@ from os.path import exists
 
 
 def do_pack():
-    """This function generates a .tgz archive from the contents of the web_static folder
-    of your AirBnB Clone repo.
+    """This function generates a .tgz archive from the contents of
+    the web_static folder of your AirBnB Clone repo.
     """
     local("mkdir -p versions")
-    path = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    path = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S"))
     local("tar -cvzf {} ./web_static".format(path))
     if (exists(path)):
         return path
