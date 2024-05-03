30aac2ae0dc495c382cdf1820d156709955fd2c2
AbduHany <abdu.hany@gmail.com>
2024-05-04 00:46:41 +0300

    Modify 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,10 @@ from datetime import datetime
 from os.path import exists
 
 
+env.hosts = ['54.157.148.186', '54.158.189.0']
+env.user = 'ubuntu'
+
+
 def do_pack():
     """This function generates a .tgz archive from the contents of
     the web_static folder of your AirBnB Clone repo.
@@ -20,22 +24,23 @@ def do_pack():
         return path
     return None
 
+
 def do_deploy(archive_path):
     """This function distributes an archive to your web servers.
     """
-    if not(exists(archive_path)):
+    if not (exists(archive_path)):
         return False
-    env.hosts = ['54.157.148.186', '54.158.189.0']
-    env.user = 'ubuntu'
     name_tgz = archive_path.split('/')[1]
     name_notgz = name_tgz.split('.')[0]
     try:
         put(archive_path, "/tmp/{}".format(name_tgz))
         sudo("mkdir -p /data/web_static/releases/{}".format(name_notgz))
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(name_tgz, name_notgz))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            name_tgz, name_notgz))
         sudo("rm /tmp/{}".format(name_tgz))
         sudo("rm /data/web_static/current")
-        sudo("ln -sf /data/web_static/releases/{} /data/web_static/current".format(name_notgz))
+        sudo("ln -sf /data/web_static/releases/{} /data/web_static/current"
+             .format(name_notgz))
     except Exception:
         return False
     return True
