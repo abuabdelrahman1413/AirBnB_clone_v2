f30e6c330889280787f31655fc1c3463aef5ebc9
AbduHany <abdu.hany@gmail.com>
2024-05-03 12:44:01 -0700

    Modify 0-setup_web_static.sh: fix sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,6 +12,6 @@ echo "<html>
      </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sudo sed -i "/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
