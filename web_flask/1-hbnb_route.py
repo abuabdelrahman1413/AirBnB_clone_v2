c135e64769037fba763bae4e9b063327286c5f67
AbduHany <abdu.hany@gmail.com>
2024-05-18 12:19:35 +0300

    Add 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ defines the page to display at '/'
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ defines the page to display at '/hbnb'
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
