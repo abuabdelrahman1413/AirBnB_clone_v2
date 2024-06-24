186656060879db7fadc99309b2d597902f68aa51
Mohammed Said <ms22abuabdelrahman1413>
2024-06-24 15:13:56 +0200

    # parse key value pairs after converting to right type

--- console.py
+++ console.py
@@ -61,12 +61,12 @@ class HBNBCommand(cmd.Cmd):
             _cls = pline[: pline.find(".")]
 
             # isolate and validate <command>
-            _cmd = pline[pline.find(".") + 1 : pline.find("(")]
+            _cmd = pline[pline.find(".") + 1: pline.find("(")]
             if _cmd not in HBNBCommand.dot_cmds:
                 raise Exception
 
             # if parantheses contain arguments, parse them
-            pline = pline[pline.find("(") + 1 : pline.find(")")]
+            pline = pline[pline.find("(") + 1: pline.find(")")]
             if pline:
                 # partition args: (<id>, [<delim>], [<*args>])
                 pline = pline.partition(", ")  # pline convert to tuple
@@ -132,11 +132,12 @@ class HBNBCommand(cmd.Cmd):
         elif command[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        # create new instance of object
+        # create new instance
         new_instance = HBNBCommand.classes[command[0]]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+
+        # parse key value pairs after converting to right type
+        index_command = 0
+        while index_command < len(command) - 1:
 
     def help_create(self):
         """Help information for the create method"""
@@ -287,7 +288,7 @@ class HBNBCommand(cmd.Cmd):
             if args and args[0] == '"':  # check for quoted arg
                 second_quote = args.find('"', 1)
                 att_name = args[1:second_quote]
-                args = args[second_quote + 1 :]
+                args = args[second_quote + 1:]
 
             args = args.partition(" ")
 
@@ -296,7 +297,7 @@ class HBNBCommand(cmd.Cmd):
                 att_name = args[0]
             # check for quoted val arg
             if args[2] and args[2][0] == '"':
-                att_val = args[2][1 : args[2].find('"', 1)]
+                att_val = args[2][1: args[2].find('"', 1)]
 
             # if att_val was not quoted arg
             if not att_val and args[2]:
