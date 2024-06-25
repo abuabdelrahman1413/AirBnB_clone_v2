b1bed11b347946a32608cd1d375e972ed4bab173
Mohammed Said <ms22abuabdelrahman1413>
2024-06-26 01:42:27 +0200

    fix an issue in all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,7 @@ class FileStorage:
         """Returns a dictionary of models in JSON format"""
         if cls:
             new_dict = {}
-            for key, value in self.__objects:
+            for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__name:
                     new_dict[key] = value
                 return new_dict
