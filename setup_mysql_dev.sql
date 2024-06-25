8a40293835e9d1cfbdae0f37d49666ee87da619b
Mohammed Said <ms22abuabdelrahman1413>
2024-06-25 14:40:21 +0200

    fix database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
--- -- Create Database if not exists
--- CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create Database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- -- Create User if not exists
--- CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create User if not exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- -- Grant All Privileges on hbnb_dev_db to hbnb_dev user
--- GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant All Privileges on hbnb_dev_db to hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- -- Grant SELECT privilege on performance_schema to hbnb_dev user
--- GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
