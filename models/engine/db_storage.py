4e93c1e191b51e17292c3c7d89c87f56d215e369
Mohammed Said <ms22abuabdelrahman1413>
2024-06-26 11:22:06 +0200

    update DB

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,7 +13,7 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import scoped_session, sessionmaker, session
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -45,3 +45,45 @@ class DBStorage:
         Returns a dict of objects
         """
         new_dict = {}
+        classes = ['State', 'City', 'User', 'Place', 'Review', 'Amenity']
+        session = self.__session
+        if cls is not None:
+            query = session.query(cls)
+            for obj in query:
+                key = obj.__class__.__name__ + '.' + obj.id
+                new_dict[key] = obj
+        else:
+            for cls_name in classes:
+                query = session.query(eval(cls_name))
+                for obj in query:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return new_dict
+
+    def new(self, obj):
+        """
+        Adds an object to the current database session
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        Commit all changes of the current database session
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        Delete from the current database session obj if not None
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        Create all tables in the database
+        """
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+        self.__session = Session
