ca6faf058c1821438d313f3930814f84d9c70b3a
Mohammed Said <ms22abuabdelrahman1413>
2024-06-26 09:57:34 +0200

    fix state.py

--- models/state.py
+++ models/state.py
@@ -21,4 +21,20 @@ class State(BaseModel, Base):
 # objects should be deleted if the parent State is deleted.
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan"
+        )
+    else:
+        @property
+        def cities(self):
+            """
+            Returns the list of City objects
+            with state_id equal to the current State.id
+            """
+            from models import storage
+            from models.city import City
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
