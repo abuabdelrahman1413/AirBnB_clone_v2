16f7b38abc9eb9d66c4f2aad3b93aec5a382d79b
Mohammed Said <ms22abuabdelrahman1413>
2024-06-26 09:15:24 +0200

    cities.py, state.py
    releate state with city

--- models/city.py
+++ models/city.py
@@ -2,10 +2,14 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
+import models
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state = relationship("State", backref="cities")
--- models/state.py
+++ models/state.py
@@ -11,5 +11,14 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
+# cities = relationship("City", backref="state", cascade="all, delete"):
+# This sets up a one-to-many relationship between State and City.
+# The cities attribute will hold all City instances that reference
+# this State instance.
+# backref="state": This creates a bidirectional relationship. Each City will
+# have a state attribute that references its parent State.
+# cascade="all, delete": This specifies that all associated City
+# objects should be deleted if the parent State is deleted.
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state", cascade="all, delete")
