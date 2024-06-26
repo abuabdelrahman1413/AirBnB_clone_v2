9fcc3b663ba46e4bd25c6d080d7c1bbdcefd8f86
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-06-26 22:53:04 +0200

    handle json engine

--- models/user.py
+++ models/user.py
@@ -9,10 +9,16 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     if storage_type == "db":
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
